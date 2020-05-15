d423f10a1fcb45651e3e3041fc6f10b05cf14d38
Jin-H-Kim <dev.jinddori@gmail.com>
2020-05-15 14:55:56 +0900

    보안정보

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const {User} = require('./models/User')
+const bodyParser = require('body-parser');
+
+//application/x-www-from-urlencoded
+app.use(bodyParser.urlencoded({extended:true}))
+//application/json
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://jin:asdfasdf@youtubeclone-6ppxl.mongodb.net/test?retryWrites=true&w=majority', {
+const config = require('./config/key');
+
+mongoose.connect(config.mongoURI , {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=>{
     console.log('Mongo DB Connect...')
@@ -11,7 +20,23 @@ mongoose.connect('mongodb+srv://jin:asdfasdf@youtubeclone-6ppxl.mongodb.net/test
     console.log(err)
 })
 
-app.get('/', (req, res) => res.send('Hello World!!'))
+app.get('/', (req, res) => res.send('Hello World!!~~ with nodemon'))
+
+app.post('/register', (req,res)=>{
+    //회원 가입 할떄 필요한 정보를 client에서 가져오면
+    //그것을 데이터 베이스에 넣는다
+    const user = new User(req.body);
+    user.save((err, doc)=>{
+        if(err){
+            return res.json({success: false, err})
+        }else{
+            return res.status(200).json({
+                success: true
+            })
+        }
+    })
+})
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
 
